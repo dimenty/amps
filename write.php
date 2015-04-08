5172bd2f6e6d9e36eee459fca9e964dfc1a0ae58
dimenty <dimentyts@gmail.com>
2015-04-08 14:17:13 +0400

    'second'

--- write.php
+++ write.php
@@ -2,7 +2,7 @@
 
 function writeConf($txt) {
     if( isset( $_POST['my_button'] ) ) {
-        file_put_contents("txt.txt", $txt, FILE_APPEND | LOCK_EX);
+        file_put_contents("testwrite.txt", $txt, FILE_APPEND | LOCK_EX);
         header('Location: localhost/amps/index.php');
         die();
     }
